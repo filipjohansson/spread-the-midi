308c33499f0d1f27ee88f93e32c468e921358411
Filip Johansson <filip@apartment5.se>
2014-05-17 17:54:26 +0200

    Updated README The Python script now accepts an argument Added a simple while True loop

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-spread-the-midi
-===============
+#Spread the MIDI#
+Create midi using a Google Spreadsheet.
+
+Created during Midi Hack 20014, Stockholm.
+
+##Usage##
+`python spreadthemidi.py [URL to spreadsheet]`
--- spreadthemidi.py
+++ spreadthemidi.py
@@ -1,5 +1,13 @@
 import time
 import rtmidi
+import sys
+
+try:
+    spreadesheet_url = sys.argv[1]
+    print spreadesheet_url
+except:
+    print 'Error: No URL given.'
+
 
 midiout = rtmidi.MidiOut()
 available_ports = midiout.get_ports()
@@ -18,3 +26,6 @@ time.sleep(0.5)
 midiout.send_message(note_off)
 
 del midiout
+
+while True:
+    # print 'This is the loop'
