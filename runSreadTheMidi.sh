11fb1c1879a6a0d0505151e1334be712e335589b
piratdavid <V052207@Davids-MacBook-Pro-2.local>
2014-05-18 12:06:01 +0200

    Running two python processes in bash script

---
+++ runSreadTheMidi.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+python spreadthemidi.py https://docs.google.com/spreadsheets/d/1b-Yv5xeDvjpA3t2EO5FEN91vMIzhH5VZokP0kQCQe7g/edit?usp=sharing 2 &
+python spreadthemidi.py https://docs.google.com/spreadsheets/d/1nSkRHbYryjZ9WYd46VJF78X0R9_jJn_LtIW_aD6lstc/edit?usp=sharing 1 &
--- spreadthemidi.py
+++ spreadthemidi.py
@@ -6,7 +6,11 @@ bpm = 120.0
 bpb = 4.0
 noteLength = float(60.0/bpm/bpb)
 bars = 16
-midiPort = 0
+midiChannels = {1: [0x90, 0x80],
+                2: [0x91, 0x81]}
+noteOnHex = 0x90
+noteOffHex = 0x80
+midiChannel = 1
 
 session = FuturesSession()
 
@@ -17,10 +21,12 @@ except:
     print 'Error: No URL given.'
 
 try:
-    midiPort = int(sys.argv[2])
-    print 'Midi port: ' + str(midiPort)
+    midiChannel = int(sys.argv[2])
+    noteOnHex = midiChannels[midiChannel][0]
+    noteOffHex = midiChannels[midiChannel][1]
+    print 'Midi channel: ' + str(midiChannel)
 except:
-    print 'Warning: No Midi port given, falling back to ' + str(midiPort)
+    print 'Warning: No Midi channel given, falling back to ' + str(midiChannel)
 
 # inputurl = "https://docs.google.com/spreadsheets/d/1poWnTI6BpJtYMmcGtOE33Kqes6yzQmw46jJwXttsPu0/edit?usp=sharing"
 slicedUrl = spreadesheet_url[39:-17]
@@ -57,15 +63,15 @@ def playNotesOnColumn(col):
             for j in data["feed"]["entry"]:
                 if(j["gs$cell"]["col"] == str(col)):
                     stopNote(j["gs$cell"]["$t"])
-    except:
-        print 'No data in spreadsheet'
+    except Exception, e:
+        print 'No data in spreadsheet, Exception: ' + str(e) 
 
 def playNote(note):
     global midiout
     notenumber = notetable[note]
     pprint(notenumber)
 
-    note_on = [0x90, notenumber, 112] # channel 1, middle C, velocity 112
+    note_on = [noteOnHex, notenumber, 112] # channel 1, middle C, velocity 112
    # note_off = [0x80, notenumber, 0]
     midiout.send_message(note_on)
     #time.sleep(noteLength)
@@ -74,7 +80,7 @@ def playNote(note):
 def stopNote(note):
     global midiout
     notenumber = notetable[note]
-    note_off = [0x80, notenumber, 0]
+    note_off = [noteOffHex, notenumber, 0]
     midiout.send_message(note_off)
 
 
@@ -83,7 +89,7 @@ available_ports = midiout.get_ports()
 
 
 if available_ports:
-    midiout.open_port(midiPort)
+    midiout.open_port(1)
 else:
     midiout.open_virtual_port("My virtual output")
 
